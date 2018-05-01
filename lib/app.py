92eb29e68a5838b411ca215473d1eff14e2da982
filipedsgn <filipe.dsgn@hotmail.com>
2018-05-01 01:46:33 -0300

    .

--- lib/app.py
+++ lib/app.py
@@ -6,27 +6,38 @@
 
 import dash
 import pandas as pd
+import os
 
 from lib import config, graficos
+from flask import send_from_directory
+
+import plotly.plotly as py
+from plotly.graph_objs import *
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from flask import send_from_directory
+import os
 
 app = dash.Dash()
 
-app.layout = html.Div(
-    html.Div([
-        html.Div([
-            html.Div(['Residência'], style={'fontSize': '22.4px', 'fontWeight': '200', 'marginLeft': '22.4px', 'lineHeight': '3em', 'display': 'block', 'lineWeight': '200', 'boxSizing': 'border-box'})
-            ], style={}
-        ),
-        html.Div([
-            html.Div(children=grafico_linha('Temperatura'), style={}),
-            html.Div([], style={}),
-            html.Div([
-                html.Div(children=grafico_linha('Humidade'), style={}),
-                html.Div([], style={}),
-                html.Div(children=grafico_linha('Extra'), style={})
-                ])
-            ])
-        ])
-    )
+app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
+
+app.layout = html.Div([
+    html.Link(rel='stylesheet', href='/static/stylesheet.css'),
+    html.Div(children="Residência", className="header"),
+    html.Div(children=grafico_linha('Temperatura'), className="grafico1"),
+    html.Div(children=grafico_linha('Humidade'), className="grafico2"),
+    html.Div(children=grafico_linha('Luminosidade'), className="grafico3")
+], className="wrapper")
+
+
+@app.server.route('/static/<path:path>')
+def static_file(path):
+    static_folder = os.path.join(os.getcwd(), 'static')
+    return send_from_directory(static_folder, path)
+
+
 if __name__ == '__main__':
     app.run_server()
