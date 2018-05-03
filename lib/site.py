247e2371ed39e5dd8e74dfedd5bfdec4dddb7224
filipedsgn <filipe.dsgn@hotmail.com>
2018-05-03 12:49:44 -0300

    .

--- lib/site.py
+++ lib/site.py
@@ -11,7 +11,7 @@ import dash_html_components as html
 from flask import send_from_directory
 import os
 
-app = dash.Dash()
+app = dash.Dash(static_folder='static')
 
 app.css.config.serve_locally = True
 app.scripts.config.serve_locally = True
@@ -32,3 +32,25 @@ def static_file(path):
 
 
 app.run_server(port=8050, host='0', debug=True)
+
+'''
+import flask    
+import os
+STATIC_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+
+@app.server.route('/static/<resource>')
+    def serve_static(resource):
+        return flask.send_from_directory(STATIC_PATH, resource)
+'''
+
+'''
+Here’s a slightly easier way… if you have a CSS file in assets/main.css then you can use the static_folder argument of dash.Dash() to serve up that folder, and then use html.Link to reference it, with an href that starts with / to make it relative to the root of your server:
+
+app = dash.Dash(__name__, static_folder='assets')
+app.scripts.config.serve_locally=True
+app.css.config.serve_locally=True
+app.layout = html.Div([
+    html.Link(href='/assets/main.css', rel='stylesheet'),
+    .....
+])
+'''
